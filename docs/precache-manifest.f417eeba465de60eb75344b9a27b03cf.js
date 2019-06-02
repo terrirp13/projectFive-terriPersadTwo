71c1c06d7929f7a6194c6cd8eab66f8590cf379c
terrirp <terri.persad@gmail.com>
2019-06-02 14:22:53 -0400

    docs ghpages

---
+++ docs/precache-manifest.f417eeba465de60eb75344b9a27b03cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f55563d1afec4c01883d550c3f8a9bb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e6e2c994fca5fe68087",
+    "url": "/static/css/main.79052b8c.chunk.css"
+  },
+  {
+    "revision": "dd2cd57d9192e6c7db09",
+    "url": "/static/js/2.dc48c606.chunk.js"
+  },
+  {
+    "revision": "3e6e2c994fca5fe68087",
+    "url": "/static/js/main.9b06d1e2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ccc05ff946b0c94b218cbed45652f6d5",
+    "url": "/static/media/manfood.ccc05ff9.svg"
+  },
+  {
+    "revision": "c5c7957b790194630b63635cd52f9b86",
+    "url": "/static/media/sky.c5c7957b.svg"
+  }
+]);
