606234d7bfdb31126aecadc07d68650f0744c173
terrirp <terri.persad@gmail.com>
2019-06-02 20:32:51 -0400

    Updates

---
+++ precache-manifest.c98fdf83241673b9aebfd41d26e1a1c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b56f3c210ac5f54aa1f5ca5ede09f6c",
+    "url": "/projectFive-terriPersadTwo/index.html"
+  },
+  {
+    "revision": "0c6dd121f7d836fefbeb",
+    "url": "/projectFive-terriPersadTwo/static/css/main.d64f8d6e.chunk.css"
+  },
+  {
+    "revision": "dd2cd57d9192e6c7db09",
+    "url": "/projectFive-terriPersadTwo/static/js/2.dc48c606.chunk.js"
+  },
+  {
+    "revision": "0c6dd121f7d836fefbeb",
+    "url": "/projectFive-terriPersadTwo/static/js/main.2d8340c3.chunk.js"
+  },
+  {
+    "revision": "9e435d1ea69fb3b7a146",
+    "url": "/projectFive-terriPersadTwo/static/js/runtime~main.225ce116.js"
+  },
+  {
+    "revision": "ccc05ff946b0c94b218cbed45652f6d5",
+    "url": "/projectFive-terriPersadTwo/static/media/manfood.ccc05ff9.svg"
+  },
+  {
+    "revision": "c5c7957b790194630b63635cd52f9b86",
+    "url": "/projectFive-terriPersadTwo/static/media/sky.c5c7957b.svg"
+  }
+]);
